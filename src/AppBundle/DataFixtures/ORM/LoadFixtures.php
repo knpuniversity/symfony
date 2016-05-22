3a540ecc99c8fa5df11b18dcb593d4047fef3de3
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:33 -0400

    Fixtures: Adding Alice yml file

--- src/AppBundle/DataFixtures/ORM/LoadFixtures.php
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -5,17 +5,12 @@ namespace AppBundle\DataFixtures\ORM;
 use AppBundle\Entity\Genus;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Nelmio\Alice\Fixtures;
 
 class LoadFixtures implements FixtureInterface
 {
     public function load(ObjectManager $manager)
     {
-        $genus = new Genus();
-        $genus->setName('Octopus'.rand(1, 100));
-        $genus->setSubFamily('Octopodinae');
-        $genus->setSpeciesCount(rand(100, 99999));
-
-        $manager->persist($genus);
-        $manager->flush();
+        $objects = Fixtures::load(__DIR__.'/fixtures.yml', $manager);
     }
 }
