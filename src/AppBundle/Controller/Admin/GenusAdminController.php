44fd3fdef6838fc3d52beb1f06b83827680ea60d
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:44 -0400

    Build Form: Very basic processing

--- src/AppBundle/Controller/Admin/GenusAdminController.php
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller\Admin;
 use AppBundle\Form\GenusFormType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * @Route("/admin")
@@ -28,9 +29,15 @@ class GenusAdminController extends Controller
     /**
      * @Route("/genus/new", name="admin_genus_new")
      */
-    public function newAction()
+    public function newAction(Request $request)
     {
         $form = $this->createForm(GenusFormType::class);
+
+        // only handles data on POST
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            dump($form->getData());die;
+        }
 
         return $this->render('admin/genus/new.html.twig', [
             'genusForm' => $form->createView()
