b70098546f80da5a2e15c8dfffd8e1673bdc17cf
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:24 -0400

    JSON: Use JsonResponse

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
@@ -34,6 +35,6 @@ class GenusController extends Controller
             'notes' => $notes
         ];
 
-        return new Response(json_encode($data));
+        return new JsonResponse($data);
     }
 }
