ee020382023a347d1d53e930178e727c56eb4d0f
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:28 -0400

    Routing: Add YAML route

---
+++ src/AppBundle/Controller/MainController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class MainController extends Controller
+{
+    public function homepageAction()
+    {
+        return $this->render('main/homepage.html.twig');
+    }
+}
