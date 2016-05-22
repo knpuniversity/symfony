c8cc6faca389a95c9a4d9d0cacdb4c5c06a5dd1d
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:42 -0400

    TwigExtension: Mark as safe

--- src/AppBundle/Twig/MarkdownExtension.php
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -16,7 +16,9 @@ class MarkdownExtension extends \Twig_Extension
     public function getFilters()
     {
         return [
-            new \Twig_SimpleFilter('markdownify', array($this, 'parseMarkdown'))
+            new \Twig_SimpleFilter('markdownify', array($this, 'parseMarkdown'), [
+                'is_safe' => ['html']
+            ])
         ];
     }
 
