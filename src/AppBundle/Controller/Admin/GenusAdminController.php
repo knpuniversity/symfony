92480b313665f2f6ff16369ffce24a36e4f1786e
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:42 -0400

    Updates: Starting admin section

---
+++ src/AppBundle/Controller/Admin/GenusAdminController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace AppBundle\Controller\Admin;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Route("/admin")
+ */
+class GenusAdminController extends Controller
+{
+    /**
+     * @Route("/genus", name="admin_genus_list")
+     */
+    public function indexAction()
+    {
+        $genuses = $this->getDoctrine()
+            ->getRepository('AppBundle:Genus')
+            ->findAll();
+
+        return $this->render('admin/genus/list.html.twig', array(
+            'genuses' => $genuses
+        ));
+    }
+
+    /**
+     * @Route("/genus/new", name="admin_genus_new")
+     */
+    public function newAction()
+    {
+        // let's go to work!
+    }
+}
