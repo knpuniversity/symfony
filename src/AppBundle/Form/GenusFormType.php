f40fcfcd47844a6bed8a103ce1fa3be3e6010261
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:45 -0400

    Customize: Use ChoiceType

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,7 +16,12 @@ class GenusFormType extends AbstractType
             ->add('subFamily')
             ->add('speciesCount')
             ->add('funFact')
-            ->add('isPublished')
+            ->add('isPublished', ChoiceType::class, [
+                'choices' => [
+                    'Yes' => true,
+                    'No' => false,
+                ]
+            ])
             ->add('firstDiscoveredAt')
         ;
     }
