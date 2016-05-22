cd3a2eedf39c6bbd5ff2f6bf248767de1e46f70a
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:40 -0400

    Cache: Using DI

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -2,20 +2,23 @@
 
 namespace AppBundle\Service;
 
+use Doctrine\Common\Cache\Cache;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 
 class MarkdownTransformer
 {
     private $markdownParser;
+    private $cache;
 
-    public function __construct(MarkdownParserInterface $markdownParser)
+    public function __construct(MarkdownParserInterface $markdownParser, Cache $cache)
     {
         $this->markdownParser = $markdownParser;
+        $this->cache = $cache;
     }
 
     public function parse($str)
     {
-        $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+        $cache = $this->cache;
         $key = md5($str);
         if ($cache->contains($key)) {
             return $cache->fetch($key);
