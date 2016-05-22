cfbf44e9e21be1f99a1dc55645f714d089638908
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:49 -0400

    User: Filling in basic User methods

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -7,23 +7,35 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 class User implements UserInterface
 {
+    private $username;
+
     public function getUsername()
     {
+        return $this->username;
     }
 
     public function getRoles()
     {
+        return ['ROLE_USER'];
     }
 
     public function getPassword()
     {
+        // leaving blank - I don't need/have a password!
     }
 
     public function getSalt()
     {
+        // leaving blank - I don't need/have a password!
     }
 
     public function eraseCredentials()
     {
+        // leaving blank - I don't need/have a password!
+    }
+
+    public function setUsername($username)
+    {
+        $this->username = $username;
     }
 }
