bf1281ba6a6bf4703b85db2f8769690da163c613
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:32 -0400

    Fixtures: Empty fixture class

--- README.md
+++ README.md
@@ -39,6 +39,7 @@ schema!
 ```
 php bin/console doctrine:database:create
 php bin/console doctrine:migrations:migrate
+php bin/console doctrine:fixtures:load
 ```
 
 If you get an error that the database exists, that should
---
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace AppBundle\DataFixtures\ORM;
+
+use AppBundle\Entity\Genus;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadFixtures implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        $genus = new Genus();
+        $genus->setName('Octopus'.rand(1, 100));
+        $genus->setSubFamily('Octopodinae');
+        $genus->setSpeciesCount(rand(100, 99999));
+
+        $manager->persist($genus);
+        $manager->flush();
+    }
+}
