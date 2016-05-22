e0bfb0094def91c2e725b926900016988e022ee8
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:21 -0400

    Templating: Render basic template

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -3,15 +3,21 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
-class GenusController
+class GenusController extends Controller
 {
     /**
      * @Route("/genus/{genusName}")
      */
     public function showAction($genusName)
     {
-        return new Response('The genus: '.$genusName);
+        $templating = $this->container->get('templating');
+        $html = $templating->render('genus/show.html.twig', array(
+            'name' => $genusName
+        ));
+
+        return new Response($html);
     }
 }
