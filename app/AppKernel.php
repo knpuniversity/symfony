c900fe7b79b00808ba8e758262a80683b9c9d73f
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:25 -0400

    Bundle: Install KnpMarkdownBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+
+            new Knp\Bundle\MarkdownBundle\KnpMarkdownBundle(),
+
             new AppBundle\AppBundle(),
         );
 
