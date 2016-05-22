495c9224f0605b6eb7290f55351ba7b454466e0f
Ryan Weaver <ryan@thatsquality.com>
2016-05-22 15:44:43 -0400

    Build Form: Create empty GenusFormType

---
+++ src/AppBundle/Form/GenusFormType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class GenusFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+
+    }
+}
